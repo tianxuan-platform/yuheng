c977ddb80ffbb07bd0e4f0d8e7f9080c56c264fe
wuyiccc <3406324191@qq.com>
2024-01-28 13:03:36 +0800

    feat: 提供heartbeat心跳接口和error接口

---
+++ src/main/java/com/wuyiccc/yuheng/controller/IndexController.java
@@ -0,0 +1,34 @@
+package com.wuyiccc.yuheng.controller;
+
+import com.wuyiccc.yuheng.infrastructure.code.ABizCode;
+import com.wuyiccc.yuheng.infrastructure.pojo.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wuyiccc
+ * @date 2024/1/28 12:54
+ */
+@Slf4j
+@Validated
+@RestController
+public class IndexController {
+
+    /**
+     * 心跳接口
+     */
+    @GetMapping("/heartbeat")
+    public R<String> login() {
+        return R.data("heartbeat");
+    }
+
+    /**
+     * 请求接口不存在的时候, mvc自动转发到该接口
+     */
+    @GetMapping("/error")
+    public R<String> error() {
+        return R.fail(ABizCode.API_NOT_FOUND);
+    }
+}
